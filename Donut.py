1892b7a831636a03758b2adcda0b0337826aceda
KeziaWahome <k.wahome@alustudent.com>
2023-12-29 00:37:19 +0200

    using matplotlib which is compatible with streamlit

--- Donut.py
+++ Donut.py
@@ -1,87 +1,98 @@
 import streamlit as st
-import pygame as pg
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import math
-import colorsys
-
-white = (255, 255, 255)
-black = (0, 0, 0)
-hue = 0
-
-WIDTH = 800
-HEIGHT = 600
-
-x_start, y_start = 0, 0
-
-x_s = 10
-y_s = 20
-
-rows = HEIGHT // y_s
-columns = WIDTH // x_s
-screen_size = rows * columns
-
-x_offset = columns / 2
-y_offset = rows / 2
 
 # Rotating animation
 A, B = 0, 0
 
-theta_spacing = 10
-phi_spacing = 5
+theta_spacing = 0.07
+phi_spacing = 0.02
 
-chars = ".,-~:;=!*#$@"
+screen_size = 40
+illumination = np.fromiter(".,-~:;=!*#$@", dtype="<U1")
 
-pg.init()
-screen = pg.Surface((WIDTH, HEIGHT))
+R1 = 1
+R2 = 2
+K2 = 5
+K1 = screen_size * K2 * 3 / (8 * (R1 + R2))
 
-@st.cache(hash_funcs={pg.Surface: lambda _: None})
-def render_donut():
-    global A, B, hue, x_start, y_start  # Add global for y_start
+# Function to render the spinning donut
+def render_donut(A, B):
+    output = np.full((screen_size, screen_size), " ")
+    zbuffer = np.zeros((screen_size, screen_size))
 
-    y_start = 0  # Initialize y_start
+    cos_phi = np.cos(phi := np.arange(0, 2 * np.pi, phi_spacing))
+    sin_phi = np.sin(phi)
+    cos_theta = np.cos(theta := np.arange(0, 2 * np.pi, theta_spacing))
+    sin_theta = np.sin(theta)
+    circle_x = R2 + R1 * cos_theta
+    circle_y = R1 * sin_theta
 
-    z = [0] * screen_size
-    b = [' '] * screen_size
+    x = (
+        np.outer(cos_B * cos_phi + sin_A * sin_B * sin_phi, circle_x)
+        - circle_y * cos_A * sin_B
+    ).T
+    y = (
+        np.outer(sin_B * cos_phi - sin_A * cos_B * sin_phi, circle_x)
+        + circle_y * cos_A * cos_B
+    ).T
+    z = (
+        (K2 + cos_A * np.outer(sin_phi, circle_x)) + circle_y * sin_A
+    ).T
+    ooz = np.reciprocal(z)
+    xp = (screen_size / 2 + K1 * ooz * x).astype(int)
+    yp = (screen_size / 2 - K1 * ooz * y).astype(int)
+    L1 = (
+        ((np.outer(cos_phi, cos_theta) * sin_B) - cos_A * np.outer(sin_phi, cos_theta))
+        - sin_A * sin_theta
+    )
+    L2 = cos_B * (cos_A * sin_theta - np.outer(sin_phi, cos_theta * sin_A))
+    L = np.around(((L1 + L2) * 8)).astype(int).T
+    mask_L = L >= 0
+    chars = illumination[L]
 
-    for j in range(0, 628, theta_spacing):
-        for i in range(0, 628, phi_spacing):
-            c = math.sin(i)
-            d = math.cos(j)
-            e = math.sin(A)
-            f = math.sin(j)
-            g = math.cos(A)
-            h = d + 2
-            D = 1 / (c * h * e + f * g + 5)
-            l = math.cos(i)
-            m = math.cos(B)
-            n = math.sin(B)
-            t = c * h * g - f * e
-            x = int(x_offset + 40 * D * (l * h * m - t * n))
-            y = int(y_offset + 20 * D * (l * h * n + t * m))
-            o = int(x + columns * y)
-            N = int(8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n))
-            if rows > y and y > 0 and x > 0 and columns > x and D > z[o]:
-                z[o] = D
-                b[o] = chars[N if N > 0 else 0]
+    for i in range(90):
+        mask = mask_L[i] & (ooz[i] > zbuffer[xp[i], yp[i]])
 
-    if y_start == rows * y_s - y_s:
-        y_start = 0
+        zbuffer[xp[i], yp[i]] = np.where(mask, ooz[i], zbuffer[xp[i], yp[i]])
+        output[xp[i], yp[i]] = np.where(mask, chars[i], output[xp[i], yp[i]])
 
-    for i in range(len(b)):
-        A += 0.00004
-        B += 0.00002
-        if i == 0 or i % columns:
-            x_start += x_s
-        else:
-            y_start += y_s
-            x_start = 0
+    return output
 
-    hue += 0.005
-
-    imgdata = pg.surfarray.array3d(screen)
-    imgdata = imgdata.swapaxes(0, 1)
-
-    return imgdata
 
 st.title("Spinning Donut")
-st.image(render_donut(), caption='Spinning Donut', use_column_width=True)
+
+# Create a Matplotlib figure
+fig, ax = plt.subplots()
+
+# Function to update the Matplotlib plot
+def update_plot(A, B):
+    ax.clear()
+    donut_frame = render_donut(A, B)
+
+    # Customize the color map for the ASCII characters
+    cmap = mcolors.ListedColormap(["black"] + list(mcolors.TABLEAU_COLORS.values()))
+    bounds = [0] + list(range(1, len(illumination) + 1))
+    norm = mcolors.BoundaryNorm(bounds, cmap.N, clip=True)
+
+    # Display the donut frame using Matplotlib's imshow
+    ax.imshow(
+        donut_frame,
+        cmap=cmap,
+        norm=norm,
+        interpolation="nearest",
+        aspect="equal",
+        extent=(-screen_size / 2, screen_size / 2, -screen_size / 2, screen_size / 2),
+    )
+
+# Streamlit slider widgets for controlling the rotation angles
+A_slider = st.slider("A", 0.0, 2 * np.pi, 0.0, step=0.01)
+B_slider = st.slider("B", 0.0, 2 * np.pi, 0.0, step=0.01)
+
+# Update the plot with the current slider values
+update_plot(A_slider, B_slider)
+
+# Display the Matplotlib figure using Streamlit
+st.pyplot(fig)
